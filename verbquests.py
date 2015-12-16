f10ea275d90ce68008b6579b0a7c3c78de21f911
Mike Lynch <mike.lynch@iinet.net.au>
2015-12-16 15:29:02 +1100

    The mustache template was (correctly) HTML-escaping single quotes, which were then coming through to twitter like &#27; - fixed. Added a command-line switch to test on a specific synset.

--- verbquests.py
+++ verbquests.py
@@ -26,10 +26,19 @@ def is_not_slur(synset):
         return True
 
 class Quest(object):
-    def __init__(self):
+    def __init__(self, synset):
         self.nouns = [ s for s in wn.all_synsets(pos=wn.NOUN) if is_not_slur(s) ]
         self.verbs = list(wn.all_synsets(pos=wn.VERB))
-        self.noun = random_word(self.nouns).upper()
+        if synset:
+            ss = wn.synset(synset)
+            if ss:
+                self.noun = random_word([ss]).upper()
+                print("Noun = %s" % self.noun)
+            else:
+                print("Couldn't find synset %s" % synset)
+                sys.exit(-1)
+        else:
+            self.noun = random_word(self.nouns).upper()
         self.verb = random_word(self.verbs).upper()
 
 def load_config(conffile):
@@ -51,8 +60,10 @@ def load_config(conffile):
 
 def dump_nouns(q):
     for s in q.nouns:
-        for l in s.lemmas():
-            print(l.name())
+        if "'" in s.name():
+            print(s.name(), s.definition())
+            for l in s.lemmas():
+                print(l.name())
 
         
 if __name__ == '__main__':
@@ -60,9 +71,10 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config', required=True, type=str, help="Config file")
     parser.add_argument('-d', '--dry-run', action='store_true', help="Don't post")
     parser.add_argument('-t', '--test', action='store_true', help="Don't post, dump list of all nouns to test for bad words")
+    parser.add_argument('-n', '--noun', type=str, help="synset id to force a noun")
     args = parser.parse_args()
     cf = load_config(args.config)
-    q = Quest()
+    q = Quest(args.noun)
     if args.test:
         dump_nouns(q)
         sys.exit(1)
